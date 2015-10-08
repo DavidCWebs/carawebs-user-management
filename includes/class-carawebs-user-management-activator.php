1f9e4cf174a12fb3386e4fdeba32e640fe58d50e
David Egan <david@carawebs.com>
2015-10-08 20:14:50 +0100

    First Commit

---
+++ includes/class-carawebs-user-management-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Carawebs_User_Management
+ * @subpackage Carawebs_User_Management/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Carawebs_User_Management
+ * @subpackage Carawebs_User_Management/includes
+ * @author     Your Name <email@example.com>
+ */
+class Carawebs_User_Management_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
